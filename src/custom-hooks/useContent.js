86dce049bfedb1eb03454175946f4297ae1948d1
Rushikesh Gaikwad <gaikwadrushikesh2505@gmail.com>
2023-05-05 13:20:27 +0530

    change components file

--- src/custom-hooks/useContent.js
+++ src/custom-hooks/useContent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import { FirebaseContext } from "../context/FirbaseContext";
 
 function useContent(target) {
