78df2287384496b97452ddb6094bb29827710336
Rushikesh Gaikwad <gaikwadrushikesh2505@gmail.com>
2023-05-06 15:12:31 +0530

    change README.md  file

---
+++ build/precache-manifest.517ab3c59c96d8b262a6d4c5f74739da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57920c03ada1c40c4dd09ddf06b559e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac730bad9dedef903a85",
+    "url": "/static/css/main.0fc28a3b.chunk.css"
+  },
+  {
+    "revision": "e256b2ae6a40ad67f491",
+    "url": "/static/js/2.be39e1b0.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.be39e1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac730bad9dedef903a85",
+    "url": "/static/js/main.ca14931e.chunk.js"
+  },
+  {
+    "revision": "1e25d1f42918f5c04d2c",
+    "url": "/static/js/runtime-main.b3f6ec1d.js"
+  }
+]);
